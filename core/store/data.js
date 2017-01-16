a244650a75fa8b594cf7fa60a34cf6325766ec84
Abhishek Kumar <labs.abhi@github.com>
2017-01-16 14:57:29 +0530

    Storage

---
+++ core/store/data.js
@@ -0,0 +1,5 @@
+const PouchDB = require('pouchdb');
+
+const ds = new PouchDB('storage');
+
+ds.info().then(info => console.log(info));
---
+++ example/index.js
@@ -0,0 +1,46 @@
+class ExampleApp {
+
+  constructor(params){}
+
+  /* application hooks */
+
+  preInstall(){}
+
+  postInstall(){}
+
+  preUninstall(){}
+
+  postUninstall(){}
+
+  /* lifecycle hooks */
+
+  preLoad(){}
+
+  postLoad(){}
+
+  preUnload(){}
+
+  postUnload(){}
+
+  /* interaction hooks */
+
+  onRouteChange(){}
+  onStateChange(state){}
+  onParamChange(params){}
+
+  doAddNotification(){}
+  doClearNotification(){}
+  doShowDialog(){}
+
+  doRender(domId){}
+
+  doUnload(){}
+  doUninstall(){}
+
+  /* accessors */
+
+  getActiveNotifications(){}
+  getState(){}
+
+
+}
